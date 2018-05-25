1cfd86317dba45686a911ae8c1d4e7cdb6c42410
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-25 21:53:45 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,10 +1,15 @@
 class Pirate
   attr_accessor :name, :height, :weight
+  @@ships = []
 
   def initialize(name, height, weight)
     @name = name
     @height = height
     @weight = weight
+  end
+
+  def self.ships
+    return @@ships
   end
 
 end
