41bb53762f8e57c967614f99461a22754cb05c5f
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-25 22:30:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,6 +12,11 @@ module FormsLab
     end
 
     post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ships = [
+        Ship.new(params[:ship_name_1],params[:ship_type_1],params[:ship_booty_1]),
+        Ship.new(params[:ship_name_2],params[:ship_type_2],params[:ship_booty_2])
+      ]
       erb :test
     end
 
