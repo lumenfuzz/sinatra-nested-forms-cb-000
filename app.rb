bf26c769ba4723a6a25bacb03e7f4ec83ebb1f17
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-25 22:01:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,10 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+      @ships = Ship.all
       erb :show
     end
 
